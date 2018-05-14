8956f84b4f4af79a68b31e4cfc2862b2a15e3c24
lotri1 <vaturis@gmail.com>
2018-05-14 22:59:25 +0300

    Question 1

--- maman13/src/Ex13.java
+++ maman13/src/Ex13.java
@@ -2,26 +2,79 @@ import static java.lang.System.out;
 
 public class Ex13 {
     public static void main(String[] args) {
-        printWeight(5358, 5);
-        printWeight(41358, 4);
-        printWeight(41358, 1);
-        printWeight(41358, 3);
-        printWeight(41358, 5);
-        printWeight(41358, 8);
-        printWeight(41358, 9);
+        printweight(5358, 5);
+        printweight(41358, 4);
+        printweight(41358, 1);
+        printweight(41358, 3);
+        printweight(41358, 5);
+        printweight(41358, 8);
+        printweight(41358, 9);
+
+        // printReverse(123456);
+        // printReverse(12345);
+        printReverse(1234);
+        printReverse(123);
+        printReverse(12);
+        printReverse(1);
+        printReverse(0);
     }
 
-    private static void printWeight(int num, int digit) {
-        out.println("weight of " + num + "," + digit + " is: " + weight(num, digit));
+    private static void printweight(int num, int digit) {
+        out.println("weight of " + num + "," + digit + " is: by not recursive: " + weight(num, digit)
+                + ", by recursive: " + weightRec(num, digit));
+    }
+
+    private static void printReverse(int num) {
+        out.println("reverse of " + num + " is: by not recursive: " + reverse(num) + ", by recursive: "
+                + reverseRec(num));
     }
 
     public static int weight(int num, int digit) {
+        int weight = 1;
+
+        while (num > 0) {
+            if (num % 10 == digit)
+                return weight;
+
+            weight *= 10;
+            num /= 10;
+        }
+
+        return 0;
+    }
+
+    public static int weightRec(int num, int digit) {
         if (num == 0)
             return 0;
 
         if (num % 10 == digit)
             return 1;
 
-        return weight(num / 10, digit) * 10;
+        return weightRec(num / 10, digit) * 10;
+    }
+
+    public static int reverse(int num) {
+        int reversedNum = 0;
+
+        while (num > 0) {
+            reversedNum *= 10;
+            reversedNum += num % 10;
+            num = num / 10;
+        }
+
+        return reversedNum;
+    }
+
+    public static int reverseRec(int num) {
+        return reverseRec(num, 0);
+    }
+
+    public static int reverseRec(int num, int reversedNum) {
+        if (num == 0)
+            return reversedNum;
+
+        reversedNum = reversedNum * 10 + num % 10;
+
+        return reverseRec(num / 10, reversedNum);
     }
 }
